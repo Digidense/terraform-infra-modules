225d24c6ed92ddb4015a292b220b1f70b33db3e9
Akhileshml <129209611+Akhileshml@users.noreply.github.com>
2024-07-25 10:38:23 +0530

    modify output.tf

--- output.tf
+++ output.tf
@@ -3,12 +3,24 @@ output "vpc_id" {
   value       = aws_vpc.digi-network.id
 }
 
-output "public_subnet" {
-  description = "The IDs of the public subnets"
-  value       = tolist(aws_subnet.pub01[*].id)
+# Outputs for each public subnet
+output "public_subnet_01" {
+  description = "The ID of the first public subnet"
+  value       = aws_subnet.pub01[0].id
 }
 
-output "private_subnet" {
-  description = "The IDs of the private subnets"
-  value       = tolist(aws_subnet.pri01[*].id)
+output "public_subnet_02" {
+  description = "The ID of the second public subnet"
+  value       = aws_subnet.pub01[1].id
+}
+
+# Outputs for each private subnet
+output "private_subnet_01" {
+  description = "The ID of the first private subnet"
+  value       = aws_subnet.pri01[0].id
+}
+
+output "private_subnet_02" {
+  description = "The ID of the second private subnet"
+  value       = aws_subnet.pri01[1].id
 }

8f8390528f85864087eab7e74533d51b9717410c
rahuldevops97 <123821605+rahuldevops97@users.noreply.github.com>
2024-08-02 16:30:04 +0530

    Update var.tf

--- S3/var.tf
+++ S3/var.tf
@@ -1,7 +1,20 @@
-variable "bucket-name" {
+# Variables for KMS
+variable "aliases_name" {
+  description = "Aliases_name for KMS"
+  type        = string
+  default     = "alias/kms_2058"
+}
+
+variable "deletion_window_in_days" {
+  description = "Deletion window in days for KMS"
+  type        = number
+  default     = 7
+}
+
+# Variables for S3
+variable "bucket_name" {
   description = "The base name of the S3 bucket"
   type        = string
-
 }
 
 variable "string_length" {
@@ -22,20 +35,55 @@ variable "string_upper" {
   default     = false
 }
 
-variable "lifecycle_expiration_days" {
-  description = "The number of days after which objects with the lifecycle prefix will expire"
-  type        = number
-  default     = 90
-}
-
-variable "kms_key_description" {
-  description = "The description of the KMS key"
+variable "bucket_acl" {
+  description = "The ACL of the S3 bucket"
   type        = string
-  default     = "KMS key for S3 encryption"
+  default     = "private"
 }
 
-variable "kms_key_deletion_window_days" {
-  description = "The deletion window in days for the KMS key"
+variable "versioning_enabled" {
+  description = "Enable versioning on the S3 bucket"
+  type        = bool
+  default     = true
+}
+
+variable "lifecycle_rule_id" {
+  description = "The ID of the lifecycle rule"
+  type        = string
+  default     = "archive"
+}
+
+variable "lifecycle_rule_enabled" {
+  description = "Enable the lifecycle rule"
+  type        = bool
+  default     = true
+}
+
+variable "transition_days_1" {
+  description = "Days after which to transition to the first storage class"
   type        = number
-  default     = 7
+  default     = 30
+}
+
+variable "storage_class_1" {
+  description = "The first storage class to transition to"
+  type        = string
+  default     = "STANDARD_IA"
+}
+
+variable "transition_days_2" {
+  description = "Days after which to transition to the second storage class"
+  type        = number
+  default     = 60
+}
+
+variable "storage_class_2" {
+  description = "The second storage class to transition to"
+  type        = string
+  default     = "GLACIER"
+}
+
+variable "use_glacier" {
+  description = "Whether to use GLACIER storage class for transition"
+  type        = bool
 }

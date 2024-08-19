0b0474499ca9e29627714d92a9f1f8d2fa74f538
rahuldevops97 <123821605+rahuldevops97@users.noreply.github.com>
2024-08-19 11:08:20 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,27 @@
-# Create an IP set (optional, for blocking specific IPs)
-resource "aws_wafv2_ip_set" "example" {
-  name               = "examplennn-ip-set"
-  description        = "Example IP set"
-  scope              = "REGIONAL"
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create an IP set for blocking specific IPs
+resource "aws_wafv2_ip_set" "digidense_ip_set" {
+  name               = var.ip_set_name
+  description        = var.ip_set_description
+  scope              = var.scope
   ip_address_version = "IPV4"
-  addresses          = ["192.0.2.0/24"]
+  addresses          = var.blocked_ips
 }
 
 # Create a WAF Web ACL
-resource "aws_wafv2_web_acl" "example" {
-  name        = "example-web-acl"
-  description = "Example Web ACL"
-  scope       = "REGIONAL" # Use 'CLOUDFRONT' for CloudFront distributions
+resource "aws_wafv2_web_acl" "digidense_web_acl" {
+  name        = var.web_acl_name
+  description = var.web_acl_description
+  scope       = var.scope
 
   default_action {
     allow {}
   }
 
+  # Rule for AWS Managed Rules Common Rule Set
   rule {
     name     = "AWS-AWSManagedRulesCommonRuleSet"
     priority = 1
@@ -39,50 +44,54 @@ resource "aws_wafv2_web_acl" "example" {
     }
   }
 
+  # Rule for Amazon IP Reputation List Managed Rule Group
+  rule {
+    name     = "AWS-AWSManagedRulesAmazonIpReputationList"
+    priority = 2
+
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesAmazonIpReputationList"
+      }
+    }
+
+    override_action {
+      none {}
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "amazonIpReputation"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Rule to block specific IPs using the IP set
+  rule {
+    name     = "BlockSpecificIPs"
+    priority = 3
+
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.digidense_ip_set.arn
+      }
+    }
+
+    action {
+      block {}
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "blockSpecificIPs"
+      sampled_requests_enabled   = true
+    }
+  }
+
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = "exampleWebACL"
+    metric_name                = var.web_acl_metric_name
     sampled_requests_enabled   = true
   }
-}
-
-# Create an API Gateway REST API
-resource "aws_api_gateway_rest_api" "example" {
-  name = "example-api"
-}
-
-# Add a GET method to the root resource of the API Gateway
-resource "aws_api_gateway_method" "root_get" {
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  resource_id   = aws_api_gateway_rest_api.example.root_resource_id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-# Create a deployment for the API Gateway
-resource "aws_api_gateway_deployment" "example" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-
-  # To force a new deployment on each change
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.example))
-  }
-
-  # Depends on the method being created
-  depends_on = [
-    aws_api_gateway_method.root_get
-  ]
-}
-
-# Create a stage for the API Gateway
-resource "aws_api_gateway_stage" "example" {
-  stage_name    = "prod"
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  deployment_id = aws_api_gateway_deployment.example.id
-}
-
-# Associate the WAF Web ACL with the API Gateway stage
-resource "aws_wafv2_web_acl_association" "example" {
-  resource_arn = "${aws_api_gateway_rest_api.example.execution_arn}/${aws_api_gateway_stage.example.stage_name}"
-  web_acl_arn  = aws_wafv2_web_acl.example.arn
 }

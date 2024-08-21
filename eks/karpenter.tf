a91f138bbf6a1aff63f09228bab97f408f538e86
Akhileshml <129209611+Akhileshml@users.noreply.github.com>
2024-08-21 16:23:46 +0530

    karpenter.tf

--- eks/karpenter.tf
+++ eks/karpenter.tf
@@ -1,35 +1,15 @@
-# Create the IAM role for Karpenter
-resource "aws_iam_role" "karpenter_role" {
-  name = "KarpenterControllerRole-${var.eks_cluster_name}"
 
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "karpenter.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-
-# Attach the required policies to the Karpenter role
-resource "aws_iam_role_policy_attachment" "karpenter_controller_policy" {
+resource "aws_iam_role_policy_attachment" "karpenter_managed_policies" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
     "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
   ])
 
-  role       = aws_iam_role.karpenter_role.name
+  role       = aws_iam_role.eks_role.name
   policy_arn = each.value
 }
 
-# Create IAM policy specific for Karpenter
 resource "aws_iam_policy" "karpenter_custom_policy" {
   name = "KarpenterCustomPolicy-${var.eks_cluster_name}"
   policy = jsonencode({
@@ -56,21 +36,25 @@ resource "aws_iam_policy" "karpenter_custom_policy" {
   })
 }
 
-# Attach the custom policy to the Karpenter role
 resource "aws_iam_role_policy_attachment" "karpenter_custom_policy_attachment" {
-  role       = aws_iam_role.karpenter_role.name
+  role       = aws_iam_role.eks_role.name
   policy_arn = aws_iam_policy.karpenter_custom_policy.arn
 }
 
+resource "kubernetes_namespace" "karpenter" {
+  metadata {
+    name = "karpenter"
+  }
+}
 
-# Deploy Karpenter using Helm
 resource "helm_release" "karpenter" {
-  name       = "karpenter"
-  repository = "https://charts.karpenter.sh"
-  chart      = "karpenter"
-  version    = "0.29.0" # Make sure to check for the latest version
-  namespace  = "karpenter"
-
+  name             = "karpenter"
+  repository       = "https://charts.karpenter.sh"
+  chart            = "karpenter"
+  version          = "v0.16.3"
+  namespace        = kubernetes_namespace.karpenter.metadata[0].name
+  create_namespace = true
+  timeout          = 1200 # Increase timeout to 20 minutes
   values = [
     <<EOF
 controller:
@@ -81,7 +65,7 @@ controller:
     create: true
     name: karpenter
     annotations:
-      eks.amazonaws.com/role-arn: ${aws_iam_role.karpenter_role.arn}
+      eks.amazonaws.com/role-arn: ${aws_iam_role.eks_role.arn}
   affinity:
     nodeAffinity:
       requiredDuringSchedulingIgnoredDuringExecution:
@@ -93,21 +77,9 @@ controller:
                   - default
 EOF
   ]
-
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.karpenter_role.arn
-  }
 }
 
-# Create namespace for Karpenter
-resource "kubernetes_namespace" "karpenter" {
-  metadata {
-    name = "karpenter"
-  }
-}
 
-# Create EKS cluster auth data
 data "aws_eks_cluster_auth" "my_cluster" {
   name = aws_eks_cluster.my_cluster.name
 }

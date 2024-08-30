cbf5400b4738e719dd3f87b7d557a6bba3640629
AnishaDevops <anishacsc2001@gmail.com>
2024-08-30 15:30:23 +0530

    Add CloudFront creation and integration

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type = string
+  default = "ud-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the bucket"
+  type = string
+  default = "mybucket-app-demo"
+}

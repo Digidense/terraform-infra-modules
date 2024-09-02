4b0d36e455d7bf430618291fe6731ec8d5d27236
Anishadevops <123820506+Anishadevops@users.noreply.github.com>
2024-09-02 16:30:03 +0530

    Update output.tf

--- waf/output.tf
+++ waf/output.tf
@@ -1,4 +1,4 @@
-output "waf_web_acl_arn" {
-  description = "The ARN of the WAF Web ACL"
-  value       = aws_wafv2_web_acl.waf_name.arn
+output "waf_name" {
+  value = aws_wafv2_web_acl.waf_name
 }
+

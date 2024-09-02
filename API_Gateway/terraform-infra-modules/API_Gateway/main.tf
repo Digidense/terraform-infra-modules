191eb98f2956f2201862990be0fd6127ff2a6a81
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-09-02 14:04:59 +0530

    API_Gateway

--- API_Gateway/terraform-infra-modules/API_Gateway/main.tf
+++ API_Gateway/terraform-infra-modules/API_Gateway/main.tf
@@ -93,7 +93,7 @@ resource "aws_api_gateway_method" "method" {
   resource_id   = aws_api_gateway_resource.api_resource.id
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   http_method   = var.method
-  authorization = "AWS_IAM"
+  authorization = "CUSTOM"
   authorizer_id = aws_api_gateway_authorizer.auth.id
 }
 

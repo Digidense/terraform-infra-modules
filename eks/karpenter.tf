44da71b571ce03d800e493cf9279c2906dcb1714
Akhileshml <129209611+Akhileshml@users.noreply.github.com>
2024-09-17 15:37:28 +0530

    Update karpenter.tf

--- eks/karpenter.tf
+++ eks/karpenter.tf
@@ -1,85 +1,80 @@
-
-resource "aws_iam_role_policy_attachment" "karpenter_managed_policies" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-  ])
-
-  role       = aws_iam_role.eks_role.name
-  policy_arn = each.value
+resource "aws_iam_role" "karpenter_controller" {
+  name = "KarpenterControllerRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-resource "aws_iam_policy" "karpenter_custom_policy" {
-  name = "KarpenterCustomPolicy-${var.eks_cluster_name}"
+resource "aws_iam_policy" "karpenter_controller_policy" {
+  name        = "KarpenterControllerPolicy"
+  path        = "/"
+  description = "Policy for Karpenter to provision and manage EC2 instances."
+
   policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version = "2012-10-17",
+    Statement = [
       {
-        "Effect" : "Allow",
-        "Action" : [
-          "ec2:CreateFleet",
-          "ec2:DescribeInstances",
+        Effect = "Allow",
+        Action = [
+          "ec2:CreateLaunchTemplate",
+          "ec2:DeleteLaunchTemplate",
+          "ec2:DescribeLaunchTemplates",
           "ec2:RunInstances",
           "ec2:TerminateInstances",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeInstanceTypes",
-          "ec2:DescribeKeyPairs",
-          "ec2:CreateTags",
-          "ec2:DescribeLaunchTemplates",
-          "iam:PassRole"
+          "eks:DescribeCluster"
         ],
-        "Resource" : "*"
+        Resource = "*"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "karpenter_custom_policy_attachment" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = aws_iam_policy.karpenter_custom_policy.arn
+resource "aws_iam_role_policy_attachment" "karpenter_controller_attach" {
+  policy_arn = aws_iam_policy.karpenter_controller_policy.arn
+  role       = aws_iam_role.karpenter_controller.name
 }
 
 resource "kubernetes_namespace" "karpenter" {
   metadata {
-    name = "karpenter"
+    name = "karpenter-a"
   }
 }
 
 resource "helm_release" "karpenter" {
-  name             = "karpenter"
-  repository       = "https://charts.karpenter.sh"
-  chart            = "karpenter"
-  version          = "v0.16.3"
-  namespace        = kubernetes_namespace.karpenter.metadata[0].name
-  create_namespace = true
-  timeout          = 1200 # Increase timeout to 20 minutes
-  values = [
-    <<EOF
-controller:
-  clusterName: ${var.eks_cluster_name}
-  clusterEndpoint: ${aws_eks_cluster.my_cluster.endpoint}
-  defaultInstanceProfile: ${var.eks_instance_profile_name}
-  serviceAccount:
-    create: true
-    name: karpenter
-    annotations:
-      eks.amazonaws.com/role-arn: ${aws_iam_role.eks_role.arn}
-  affinity:
-    nodeAffinity:
-      requiredDuringSchedulingIgnoredDuringExecution:
-        nodeSelectorTerms:
-          - matchExpressions:
-              - key: karpenter.sh/provisioner-name
-                operator: In
-                values:
-                  - default
-EOF
-  ]
+  name       = "karpenter"
+  namespace  = kubernetes_namespace.karpenter.metadata[0].name
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  version    = "v0.16.3"
+
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.karpenter_controller.arn
+  }
+
+  set {
+    name  = "clusterName"
+    value = var.eks_cluster_name
+  }
+
+  set {
+    name  = "clusterEndpoint"
+    value = aws_eks_cluster.my_cluster.endpoint
+  }
+
+  set {
+    name  = "awsRegion"
+    value = "us-east-1"
+  }
+
 }
 
 
-data "aws_eks_cluster_auth" "my_cluster" {
-  name = aws_eks_cluster.my_cluster.name
-}
